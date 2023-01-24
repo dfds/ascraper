7dea6470bb7bc98507609baaacda88e1859ba4e9
Emil H. Clausen <sequoiia@hummel.yt>
2023-01-24 10:52:55 +0100

    ... Listing services in K8s

--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/utils/env"
+	"log"
 	"time"
 )
 
 func main() {
-	fmt.Println("Hi. I don't do jack ****.")
-	time.Sleep(time.Hour * 1000)
+	client, err := getK8sClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		svcs, err := client.CoreV1().Services("selfservice").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, svc := range svcs.Items {
+			fmt.Println(svc.Name)
+			fmt.Println(svc.Spec)
+		}
+
+		fmt.Println("zzzz")
+		time.Sleep(time.Second * 60)
+	}
+}
+
+func getK8sClient() (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", env.GetString("KUBECONFIG", ""))
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
